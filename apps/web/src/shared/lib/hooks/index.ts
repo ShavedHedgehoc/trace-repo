a818c3f660c4f4007bcced468365d0fd1f5f1fdf
ShavedHedgehoc <eagle221174@gmail.com>
2026-05-07 01:35:13 +0300

    fix: working sidebar

--- apps/web/src/shared/lib/hooks/index.ts
+++ apps/web/src/shared/lib/hooks/index.ts
@@ -1,1 +1,2 @@
-export * from "./use-route-title"
+export * from "./use-route-title";
+export * from "./use-mobile"
--- apps/web/src/widgets/sidebar/sidebar.tsx
+++ apps/web/src/widgets/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 // components/app-sidebar.tsx
-import { ChevronRight, FileText, Settings, Users } from 'lucide-react';
+import { ChevronRight, Command, FileText, Settings, Users } from 'lucide-react';
 import {
   Sidebar,
   SidebarContent,
@@ -11,42 +11,70 @@ import {
   SidebarMenuSub,
   SidebarMenuSubItem,
   SidebarMenuSubButton,
+  SidebarHeader,
+  SidebarFooter,
 } from '@/shared/ui';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/shared/ui';
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
+    // <Sidebar collapsible="offcanvas" {...props}>
+    //   <SidebarContent>
+    //     <SidebarGroup>
+    //       <SidebarGroupLabel>Навигация</SidebarGroupLabel>
+    //       <SidebarMenu>
+    //         {/* Пример вложенного меню */}
+    //         <Collapsible className="group/collapsible">
+    //           <SidebarMenuItem>
+    //             <CollapsibleTrigger>
+    //               <span>
+    //                 <SidebarMenuButton  >
+    //                   <FileText />
+    //                   <span>Документы</span>
+    //                   <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+    //                 </SidebarMenuButton>
+    //               </span>
+    //             </CollapsibleTrigger>
+    //             <CollapsibleContent>
+    //               <SidebarMenuSub>
+    //                 <SidebarMenuSubItem>
+    //                   <SidebarMenuSubButton>
+    //                     <a href="/docs/all">Все файлы</a>
+    //                   </SidebarMenuSubButton>
+    //                 </SidebarMenuSubItem>
+    //               </SidebarMenuSub>
+    //             </CollapsibleContent>
+    //           </SidebarMenuItem>
+    //         </Collapsible>
+    //       </SidebarMenu>
+    //     </SidebarGroup>
+    //   </SidebarContent>
+    // </Sidebar>
     <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
+            >
+              <a href="#">
+                <Command className="size-5!" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Навигация</SidebarGroupLabel>
-          <SidebarMenu>
-            {/* Пример вложенного меню */}
-            <Collapsible className="group/collapsible">
-              <SidebarMenuItem>
-                <CollapsibleTrigger>
-                  <span>
-                    <SidebarMenuButton  >
-                      <FileText />
-                      <span>Документы</span>
-                      <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
-                    </SidebarMenuButton>
-                  </span>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    <SidebarMenuSubItem>
-                      <SidebarMenuSubButton>
-                        <a href="/docs/all">Все файлы</a>
-                      </SidebarMenuSubButton>
-                    </SidebarMenuSubItem>
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              </SidebarMenuItem>
-            </Collapsible>
-          </SidebarMenu>
-        </SidebarGroup>
+        {/* <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
       </SidebarContent>
+      <SidebarFooter>
+        ffffff
+        {/* <NavUser user={data.user} /> */}
+      </SidebarFooter>
     </Sidebar>
   );
 }
