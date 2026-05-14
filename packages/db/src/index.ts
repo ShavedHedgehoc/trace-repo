c9f688d59c39ffc575195913822b9d48ea5fd96f
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-05-14 15:55:42 +0300

    build:20

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,3 +3,5 @@ import { PrismaClient, Prisma } from './generated/client';
 export const mssqlPrisma = new PrismaClient();
 export { Prisma };
 export * from './generated';
+
+export * from './generated/index.js';
